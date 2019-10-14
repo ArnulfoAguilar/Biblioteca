e233263a9be8c95d34ee3bb01833232671056a15
Luis Valladares <valladares.3309@gmail.com>
2019-10-13 23:00:07 -0600

    migracion de vista para el listado de prestamos

---
+++ database/migrations/2019_10_13_225750_create_prestamos_view.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePrestamosView extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        DB::statement('
+            CREATE OR REPLACE VIEW public.vwprestamos
+            AS SELECT p.id,
+                p."FECHA_PRESTAMO",
+                ep."ESTADO_PRESTAMO",
+                e."EJEMPLAR",
+                u."name"
+            FROM "Prestamo" p
+                JOIN users u ON p."ID_USUARIO" = u.id
+                JOIN "estadoPrestamo" ep ON p."ID_ESTADO_PRESTAMO" = ep.id
+                JOIN "materialBibliotecario" mb ON p."ID_MATERIAL" = mb.id
+                JOIN "Ejemplar" e ON mb."ID_EJEMPLAR" = e.id;
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prestamos_view');
+    }
+}
