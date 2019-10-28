0d4899748e2cc0728bddb13e051a775006cb4e7e
Kevin <kposada1771@gmail.com>
2019-10-27 21:20:05 -0600

    Poniendo en contexto
    Hice la clase de prestamo tal y como esta en la Rama de Luis

--- app/Prestamo.php
+++ app/Prestamo.php
@@ -1,10 +1,18 @@
 <?php
 
-namespace App;
+namespace App\Modelos;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Prestamo extends Model
 {
-    //
+    protected $table = "Prestamo";
+    protected $fillable = [
+        'FECHA_PRESTAMO',
+        'ID_USUARIO',
+        'ID_TIPO_PRESTAMO',
+        'ID_ESTADO_PRESTAMO',
+        'ID_MATERIAL'
+
+    ];
 }
