3b7398aaa2a93c436350d384ee7fef8fc9a02cb0
Kevin <kposada1771@gmail.com>
2019-10-28 23:01:17 -0600

    Minimo

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-
+        $this->call(RolesTableSeeder::class);
+        
         $this->call(UserTableSeeder::class);
         $this->call(RevisionTableSeeder::class);
         $this->call(AreaTableSeeder::class);
         $this->call(TipoAporteTableSeeder::class);
         $this->call(PalabrasClaveTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
         $this->call(tipoInteraccion::class);
         $this->call(ComiteTableSeeder::class);
         $this->call(PalabraProhibidaTableSeeder::class);
