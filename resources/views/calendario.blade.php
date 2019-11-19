e92f256d578837da4b70d5ecdc00ca4c30a1de08
Kevin Posada <kposada1771@gmail.com>
2019-11-19 16:17:44 -0600

    Mejoras

--- resources/views/calendario.blade.php
+++ resources/views/calendario.blade.php
@@ -1,6 +1,12 @@
 @extends('layouts.adminLTE')
 
 @section('content')
-    <h1>aquí iría el calendario</h1>
+    {{-- <h1>aquí iría el calendario</h1> --}}
+    <div class="card">
+        <div class="card-header">
+            En este espacio se asignan en el calendario los dias de asueto o dias no hábiles; 
+            Con esto el sistema estimará una fecha adecuada para la devolución de un préstamo
+        </div>
+    </div>
     <calendario></calendario>
 @endsection
