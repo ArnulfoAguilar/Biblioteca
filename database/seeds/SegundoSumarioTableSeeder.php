19ce0fdbeaa494df14e5257c636265bce42042ff
Kevin <kposada1771@gmail.com>
2020-01-10 10:49:40 -0600

    Minimo cambio en sumarios

--- database/seeds/SegundoSumarioTableSeeder.php
+++ database/seeds/SegundoSumarioTableSeeder.php
@@ -11,30 +11,10 @@ class SegundoSumarioTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('segundoSumario')->insert([
-            'id' => 1,
-            'DESCRIPCION' => '110 - Metafísica',
-            'ID_PRIMER_SUMARIO' => 2
-        ]);
-        DB::table('segundoSumario')->insert([
-            'id' => 2,
-            'DESCRIPCION' => '120 - Epistemología',
-            'ID_PRIMER_SUMARIO' => 2
-        ]);
-        DB::table('segundoSumario')->insert([
-            'id' => 3,
-            'DESCRIPCION' => '150 - Psicología',
-            'ID_PRIMER_SUMARIO' => 2
-        ]);
-        DB::table('segundoSumario')->insert([
-            'id' => 4,
-            'DESCRIPCION' => '220 - La Biblia',
-            'ID_PRIMER_SUMARIO' => 3
-        ]);
-        DB::table('segundoSumario')->insert([
-            'id' => 5,
-            'DESCRIPCION' => '230 - Cristianismo; Teología Cristiana',
-            'ID_PRIMER_SUMARIO' => 3
-        ]);
+        DB::table('segundoSumario')->insert(['id' => 1, 'DESCRIPCION' => '110 - Metafísica', 'ID_PRIMER_SUMARIO' => 2 ]);
+        DB::table('segundoSumario')->insert(['id' => 2, 'DESCRIPCION' => '120 - Epistemología',  'ID_PRIMER_SUMARIO' => 2 ]);
+        DB::table('segundoSumario')->insert(['id' => 3, 'DESCRIPCION' => '150 - Psicología', 'ID_PRIMER_SUMARIO' => 2 ]);
+        DB::table('segundoSumario')->insert(['id' => 4, 'DESCRIPCION' => '220 - La Biblia',  'ID_PRIMER_SUMARIO' => 3 ]);
+        DB::table('segundoSumario')->insert(['id' => 5, 'DESCRIPCION' => '230 - Cristianismo; Teología Cristiana', 'ID_PRIMER_SUMARIO' => 3 ]);
     }
 }
