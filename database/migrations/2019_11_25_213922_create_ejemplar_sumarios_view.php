ba483bd8c40d018d43705ce0472b89182d9bd975
Luis Valladares <valladares.3309@gmail.com>
2020-01-12 19:18:23 -0600

    modificacion de migracion de la vista ejemplar sumarios

--- database/migrations/2019_11_25_213922_create_ejemplar_sumarios_view.php
+++ database/migrations/2019_11_25_213922_create_ejemplar_sumarios_view.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
 class CreateEjemplarSumariosView extends Migration
 {
@@ -13,60 +14,66 @@ class CreateEjemplarSumariosView extends Migration
     public function up()
     {
         DB::statement('
-            CREATE OR REPLACE VIEW public.vwejemplarsumarios as
-            SELECT e.id,
-            e."ISBN",
-            e."EJEMPLAR",
-            e."SUBTITULO",
-            e."EDITORIAL",
-            e."EDICION",
-            e."AÑO_EDICION",
-            e."LUGAR_EDICION",
-            e."OBSERVACIONES",
-            e."DESCRIPCION",
-            e."NUMERO_PAGINAS",
-            e."NUMERO_COPIAS",
-            e."AUTOR",
-            e."ID_AREA",
-            e."ID_TIPO_EMPASTADO",
-            e."ID_TIPO_ADQUISICION",
-            e."ID_ESTADO_EJEMPLAR",
-            e."ID_CATALOGO_MATERIAL",
-            ts.id AS "ID_TERCER_SUMARIO",
-            ss.id AS "ID_SEGUNDO_SUMARIO",
-            ps.id AS "ID_PRIMER_SUMARIO",
-            e.deleted_at,
-                e."PRECIO"
-           FROM (((( SELECT "Ejemplar".id,
-                    "Ejemplar"."ISBN",
-                    "Ejemplar"."EJEMPLAR",
-                    "Ejemplar"."SUBTITULO",
-                    "Ejemplar"."EDITORIAL",
-                    "Ejemplar"."EDICION",
-                    "Ejemplar"."AÑO_EDICION",
-                    "Ejemplar"."LUGAR_EDICION",
-                    "Ejemplar"."OBSERVACIONES",
-                    "Ejemplar"."IMAGEN",
-                    "Ejemplar"."DESCRIPCION",
-                    "Ejemplar"."PALABRAS_CLAVE",
-                    "Ejemplar"."NUMERO_PAGINAS",
-                    "Ejemplar"."NUMERO_COPIAS",
-                    "Ejemplar"."AUTOR",
-                    "Ejemplar"."ID_AREA",
-                    "Ejemplar"."ID_TERCER_SUMARIO",
-                    "Ejemplar"."ID_TIPO_EMPASTADO",
-                    "Ejemplar"."ID_TIPO_ADQUISICION",
-                    "Ejemplar"."ID_ESTADO_EJEMPLAR",
-                    "Ejemplar"."ID_CATALOGO_MATERIAL",
-                    "Ejemplar".created_at,
-                    "Ejemplar".updated_at,
-                    "Ejemplar".deleted_at,
-                                  "PRECIO"
-                   FROM "Ejemplar"
-                  WHERE ("Ejemplar".deleted_at IS NULL)) e
-             LEFT JOIN "tercerSumario" ts ON ((e."ID_TERCER_SUMARIO" = ts.id)))
-             LEFT JOIN "segundoSumario" ss ON ((ts."ID_SEGUNDO_SUMARIO" = ss.id)))
-             LEFT JOIN "primerSumario" ps ON ((ss."ID_PRIMER_SUMARIO" = ps.id)))
+            CREATE OR REPLACE VIEW public.vwejemplarsumarios
+AS SELECT e.id,
+    e."ISBN",
+    e."EJEMPLAR",
+    e."SUBTITULO",
+    e."EDITORIAL",
+    e."EDICION",
+    e."AÑO_EDICION",
+    e."LUGAR_EDICION",
+    e."OBSERVACIONES",
+    e."DESCRIPCION",
+    e."NUMERO_PAGINAS",
+    e."NUMERO_COPIAS",
+    e."AUTOR",
+    e."ID_AREA",
+    e."ID_TIPO_EMPASTADO",
+    e."ID_TIPO_ADQUISICION",
+    e."ID_ESTADO_EJEMPLAR",
+    e."ID_CATALOGO_MATERIAL",
+    ts.id AS "ID_TERCER_SUMARIO",
+    ss.id AS "ID_SEGUNDO_SUMARIO",
+    ps.id AS "ID_PRIMER_SUMARIO",
+    e.deleted_at,
+    e."PRECIO",
+    e."ID_ESTANTE",
+    b.id as "ID_BIBLIOTECA",
+    e."IMAGEN"
+   FROM ( SELECT "Ejemplar".id,
+            "Ejemplar"."ISBN",
+            "Ejemplar"."EJEMPLAR",
+            "Ejemplar"."SUBTITULO",
+            "Ejemplar"."EDITORIAL",
+            "Ejemplar"."EDICION",
+            "Ejemplar"."AÑO_EDICION",
+            "Ejemplar"."LUGAR_EDICION",
+            "Ejemplar"."OBSERVACIONES",
+            "Ejemplar"."IMAGEN",
+            "Ejemplar"."DESCRIPCION",
+            "Ejemplar"."PALABRAS_CLAVE",
+            "Ejemplar"."NUMERO_PAGINAS",
+            "Ejemplar"."NUMERO_COPIAS",
+            "Ejemplar"."AUTOR",
+            "Ejemplar"."ID_AREA",
+            "Ejemplar"."ID_TERCER_SUMARIO",
+            "Ejemplar"."ID_TIPO_EMPASTADO",
+            "Ejemplar"."ID_TIPO_ADQUISICION",
+            "Ejemplar"."ID_ESTADO_EJEMPLAR",
+            "Ejemplar"."ID_CATALOGO_MATERIAL",
+            "Ejemplar".created_at,
+            "Ejemplar".updated_at,
+            "Ejemplar".deleted_at,
+            "Ejemplar"."PRECIO",
+            "Ejemplar"."ID_ESTANTE"
+           FROM "Ejemplar"
+          WHERE "Ejemplar".deleted_at IS NULL) e
+     LEFT JOIN "tercerSumario" ts ON e."ID_TERCER_SUMARIO" = ts.id
+     LEFT JOIN "segundoSumario" ss ON ts."ID_SEGUNDO_SUMARIO" = ss.id
+     LEFT JOIN "primerSumario" ps ON ss."ID_PRIMER_SUMARIO" = ps.id
+    left join "Estante" est on e."ID_ESTANTE" = est.id
+    left join "Biblioteca" b on est."ID_BIBLIOTECA" = b.id;
         ');
     }
 
