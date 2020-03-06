f03f364c1bca63c9135ee042642fd4e8eeea8e4c
keepercito <keepercito@gmail.com>
2020-03-05 20:44:42 -0600

    fix sidebar en penalizaciones

--- resources/views/Penalizacion/penalizaciones.blade.php
+++ resources/views/Penalizacion/penalizaciones.blade.php
@@ -30,93 +30,91 @@
                             </form> --}}
                         </div>
                     </div>
-                    
+
                     <div class="card-body">
+                        <div class="d-flex justify-content-center">
+                            {{-- {{$registros->appends([
+                                'fecha_i' => $fecha_i,
+                                'fecha_f' => $fecha_f
+                                ])->links()}} --}}
+
+                                {{-- {{$penalizaciones->links()}} --}}
+                        </div>
+
+                        <table class="table table-hover table-bordered" id="penalizaciones">
+                            <thead>
+                                <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Usuario reportado</th>
+                                <th scope="col">Préstamo involucrado</th>
+                                <th scope="col">Material</th>
+                                <th scope="col">Copia</th>
+                                <th scope="col">Devolución esperada</th>
+                                <th scope="col">Penalización</th>
+                                <th scope="col">Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @if(sizeof($penalizaciones) <= 0)
+                                    <tr>
+                                        <td>--</td>
+                                        <td class="text-center" colspan="7">No Hay Penalizaciones Realizadas</td>
+                                    </tr>
+                                @else
+                                    @foreach ($penalizaciones as $penalizacion)
+                                        <tr>
+                                            <td>{{$penalizacion->id}}</td>
+                                            <td>{{$penalizacion->prestamo->usuario->name}}</td>
+                                            <td>{{$penalizacion->prestamo->id}}</td>
+                                            <td>
+                                                @foreach ($penalizacion->prestamo->materiales as $material)
+                                                    <div> {{$material->ejemplar->EJEMPLAR}}</div>
+                                                @endforeach
+                                                {{-- {{$penalizacion->prestamo->material->ejemplar->EJEMPLAR}} --}}
+                                            </td>
+                                            <td>
+                                                @foreach ($penalizacion->prestamo->materiales as $material)
+                                                    <div> {{$material->COPIA_NUMERO}}</div>
+                                                @endforeach
+                                                {{-- {{$penalizacion->prestamo->material->COPIA_NUMERO}} --}}
+                                            </td>
+                                            <td>{{ date('d-m-Y', strtotime( $penalizacion->prestamo->FECHA_ESPERADA_DEVOLUCION )) }}</td>
+                                            <td>{{$penalizacion->tipo->TIPO_PENALIZACION}}</td>
+                                            <td>
+                                                @if ($penalizacion->SOLVENTADA == true)
+                                                    <div class="badge bg-success">
+                                                        Solventada
+                                                    </div>
+                                                @else
+                                                    <button class="btn  btn-info solventar" title="Solventar" data-pena="{{$penalizacion->id}}"><i class="fas fa-hands-helping"></i></button>
+                                                @endif
+                                            </td>
+                                        </tr>
+                                    @endforeach
+
+                                @endif
+
+                            </tbody>
+                            <tfoot>
+
+                            </tfoot>
+
+                            </table>
+
                             <div class="d-flex justify-content-center">
                                 {{-- {{$registros->appends([
                                     'fecha_i' => $fecha_i,
                                     'fecha_f' => $fecha_f
                                     ])->links()}} --}}
-
                                     {{-- {{$penalizaciones->links()}} --}}
+
                             </div>
 
-                            <table class="table table-hover table-bordered" id="penalizaciones">
-                                <thead>
-                                  <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Usuario reportado</th>
-                                    <th scope="col">Préstamo involucrado</th>
-                                    <th scope="col">Material</th>
-                                    <th scope="col">Copia</th>
-                                    <th scope="col">Devolución esperada</th>
-                                    <th scope="col">Penalización</th>
-                                    <th scope="col">Acciones</th>
-                                  </tr>
-                                </thead>
-                                <tbody>
-                                    @if(sizeof($penalizaciones) <= 0)
-                                        <tr>
-                                            <td>--</td>
-                                            <td class="text-center" colspan="7">No Hay Penalizaciones Realizadas</td>
-                                        </tr>
-                                    @else
-                                        @foreach ($penalizaciones as $penalizacion)
-                                            <tr>
-                                                <td>{{$penalizacion->id}}</td>
-                                                <td>{{$penalizacion->prestamo->usuario->name}}</td>
-                                                <td>{{$penalizacion->prestamo->id}}</td>
-                                                <td>
-                                                    @foreach ($penalizacion->prestamo->materiales as $material)
-                                                        <div> {{$material->ejemplar->EJEMPLAR}}</div>
-                                                    @endforeach
-                                                    {{-- {{$penalizacion->prestamo->material->ejemplar->EJEMPLAR}} --}}
-                                                </td>
-                                                <td>
-                                                    @foreach ($penalizacion->prestamo->materiales as $material)
-                                                        <div> {{$material->COPIA_NUMERO}}</div>
-                                                    @endforeach
-                                                    {{-- {{$penalizacion->prestamo->material->COPIA_NUMERO}} --}}
-                                                </td>
-                                                <td>{{ date('d-m-Y', strtotime( $penalizacion->prestamo->FECHA_ESPERADA_DEVOLUCION )) }}</td>
-                                                <td>{{$penalizacion->tipo->TIPO_PENALIZACION}}</td>
-                                                <td>
-                                                    @if ($penalizacion->SOLVENTADA == true)
-                                                        <div class="badge bg-success">
-                                                            Solventada
-                                                        </div>
-                                                    @else
-                                                        <button class="btn  btn-info solventar" title="Solventar" data-pena="{{$penalizacion->id}}"><i class="fas fa-hands-helping"></i></button>
-                                                    @endif
-                                                </td>
-                                            </tr>
-                                        @endforeach
-                                        
-                                    @endif
-                                   
-                                </tbody>
-                                <tfoot>
-                                    
-                                </tfoot>
-                                
-                              </table>
-                              
-                              <div class="d-flex justify-content-center">
-                                    {{-- {{$registros->appends([
-                                        'fecha_i' => $fecha_i,
-                                        'fecha_f' => $fecha_f
-                                        ])->links()}} --}}
-                                        {{-- {{$penalizaciones->links()}} --}}
 
-                                </div>
-                               
-                            
-                                
-                            <div class="modal-footer">
-                                
-                            </div>
+
+                        <div class="modal-footer">
+
                         </div>
-                        
                     </div>
                 </div>
             </div>
@@ -127,11 +125,11 @@
 @section('jsExtra')
 
     <link rel="stylesheet" type="text/css" href="/DataTables/datatables.css">
-    
+
     <script type="text/javascript" charset="utf8" src="/DataTables/datatables.js"></script>
 
     <script type="text/javascript">
-    
+
         $(document).ready( function () {
             $('#penalizaciones').DataTable({
                 "order": [[ 0, "desc" ]]
